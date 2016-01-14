c727c662dbf842484e159de3ed841fb2a4d0aac2
Mel Sovereign <melsovereign@yahoo.com>
2016-01-14 08:26:11 -0500

    Conveyor belts receives drive

--- Assets/Scripts/Cog/Drivable/ConveyorBelt.cs
+++ Assets/Scripts/Cog/Drivable/ConveyorBelt.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 using System;
 
 public class ConveyorBelt : Drivable , ICollisionProxyClient
 {
     protected LineSegment lineSegment;
     protected float _radius;
+    protected List<Collision> collisions;
+    protected float wheelRotation;
+
     protected override float radius {
         get {
             if (_radius == 0f) {
@@ -17,13 +20,25 @@ public class ConveyorBelt : Drivable , ICollisionProxyClient
 
     protected float beltSpeed {
         get {
-            return angleStep.deltaAngle * radius;
+            return wheelRotation * radius;
         }
     }
 
     protected override void awake() {
         base.awake();
         lineSegment = GetComponentInChildren<LineSegment>();
+        collisions = new List<Collision>();
+    }
+
+    protected override void update() {
+        base.update();
+    }
+
+    void FixedUpdate() {
+        foreach (Collision coll in collisions) {
+            coll.rigidbody.MovePosition(coll.transform.position + lineSegment.normalized.vector3() * beltSpeed);
+        }
+
     }
 
     public override float driveScalar() {
@@ -31,21 +46,24 @@ public class ConveyorBelt : Drivable , ICollisionProxyClient
     }
 
     public override Drive receiveDrive(Drive drive) {
-// TODO: update appearance of the belt?
+        // TODO: update appearance of the belt?
+        // TODO: set up collider dropper and cursor agent. Test drivability of belt
+        wheelRotation = drive.amount * -1f / radius;
         return Drive.Zero;
     }
 
     public void proxyCollisionEnter(Collision collision) {
         collision.rigidbody.velocity = Vector3.zero;
-        
+        collisions.Add(collision);
     }
 
     public void proxyCollisionStay(Collision collision) {
         //collision.rigidbody.AddForce(lineSegment.normalized.vector3() * .5f);
         //TODO: want simple move by instead
+        //collision.rigidbody.MovePosition(collision.transform.position + lineSegment.normalized.vector3() * .2f);
     }
 
     public void proxyCollisionExit(Collision collision) {
-        print("c exit");
+        collisions.Remove(collision);
     }
 }
